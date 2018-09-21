dd21cebc08668d0e5b77824d03c294c22e3c0006
gerosantacruz <geronimo.santacruz2017@gmail.com>
2018-09-21 16:28:25 -0300

    Actividad obligatoria numero 1

---
+++ Documents/NetBeansProjects/EjercicioNumero1/src/ejercicionumero1/EjercicioNumero1.java
@@ -0,0 +1,119 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicionumero1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Gero Santacruz
+ */
+public class EjercicioNumero1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        BufferedReader bufEntrada = new BufferedReader(new InputStreamReader(System.in));
+//		double a;
+//		double b;
+//		double division;
+//		double multi;
+//		double resta;
+//		double suma;
+//		System.out.println("Ingrese el primer numero");
+//		a = Integer.parseInt(bufEntrada.readLine());
+//		System.out.println("Ingrese el segundo numero");
+//		b = Integer.parseInt(bufEntrada.readLine());
+//		suma = a+b;
+//		resta = a-b;
+//		division = a/b;
+//		multi = a*b;
+//		System.out.println("la suma de los dos numero es "+suma);
+//		System.out.println("la resta de los dos numero es "+resta);
+//		System.out.println("la division de los dos numero es "+division);
+//		System.out.println("la multiplicacion de los dos numero es "+multi);
+//                
+      //Ejercicio numero 2
+//      BufferedReader bufEntrada = new BufferedReader(new InputStreamReader(System.in));
+//		int a;
+//		int b;
+//		int c;
+//		int temp;
+//		System.out.println("ingrese el primer numero");
+//		a = Integer.parseInt(bufEntrada.readLine());
+//		System.out.println("Ingrese el segundo numero");
+//		b = Integer.parseInt(bufEntrada.readLine());
+//		System.out.println("Ingrese el tercer numero");
+//		c = Integer.parseInt(bufEntrada.readLine());
+//		System.out.println("El orden de los numero ingresados son a:"+a+" b:"+b+" c:"+c);
+//		temp = a;
+//		a = c;
+//		c = temp;
+//		System.out.println("Ahora orden de los numero ingresados son a:"+a+" b:"+b+" c:"+c);
+                
+//      //Ejercicio Numero 3
+        //      double j;
+        //      int total;
+        //      total = 0;
+        //      for (j=1;j<=10;j++) {
+        //          total = (int) (total+j);
+        //	}
+        //      System.out.println("La suma de los 10 primero numeros naturales es: "+total);
+
+//Ejercicio 4
+//    BufferedReader bufEntrada = new BufferedReader(new InputStreamReader(System.in));
+//		int alumnobarones;
+//		int alumnomujeres;
+//		double porchombres;
+//		double pormujeres;
+//		System.out.println("Ingresar cantidad de alumnos hombres");
+//		alumnobarones = Integer.parseInt(bufEntrada.readLine());
+//		System.out.println("Ingresar cantidad de alumnos mujeres");
+//		alumnomujeres = Integer.parseInt(bufEntrada.readLine());
+//		porchombres = (alumnobarones/(alumnomujeres+alumnobarones))*100;
+//		System.out.println("La cantidad de alumnos varones son "+
+//                        Math.round(porchombres)+"%");
+//		pormujeres = (alumnomujeres/(alumnomujeres+alumnobarones))*100;
+//		System.out.println("La cantidad de alumnos mujeres son "+
+//                        Math.round(pormujeres)+"%");
+
+//Ejercicio 5
+//              int ar[][];
+//              int i;
+//		int j;
+//		double prom;
+//		double suma;
+//		ar = new int[1][10];
+//		System.out.println("Ingrese los 10 numeros a sumar y su promedio");
+//		for (j=1;j<=10;j++) {
+//			System.out.println("Ingrese numero "+j+":");
+//			ar[0][j-1] = Integer.parseInt(bufEntrada.readLine());
+//		}
+//		suma = 0;
+//		for (i=1;i<=10;i++) {
+//			suma = suma+ar[0][i-1];
+//		}
+//		System.out.println("La suma total es "+suma);
+//		prom = suma/10;
+//		System.out.println("El promedio de los numero son "+prom);
+
+
+    
+                
+                
+        
+        
+    }
+    
+}
