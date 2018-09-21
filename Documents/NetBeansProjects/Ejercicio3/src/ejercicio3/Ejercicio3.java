c3fba03ed6a797b3c845a76ae53f932f591effee
gerosantacruz <geronimo.santacruz2017@gmail.com>
2018-09-21 16:29:19 -0300

    Actividad obligatoria numero 3

---
+++ Documents/NetBeansProjects/Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author Gero Santacruz
+ */
+public class Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+	double j;
+	int total;
+	total = 0;
+	for (j=1;j<=10;j++) {
+		total = (int) (total+j);
+		}
+	System.out.println("La suma de los 10 primero numeros naturales es: "+total);
+    }
+    
+}
