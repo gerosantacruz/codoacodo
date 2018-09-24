e8e4e8bdb84e4e08023cffac800cf38f16f2f4e1
gerosantacruz <geronimo.santacruz2017@gmail.com>
2018-09-24 10:58:44 -0300

    Actividad obligatoria numero 4 parte 3

---
+++ Documents/NetBeansProjects/ActividadObligatoria4Parte3/src/actividadobligatoria4parte3/ActividadObligatoria4Parte3.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividadobligatoria4parte3;
+
+/**
+ *
+ * @author Gero Santacruz
+ */
+public class ActividadObligatoria4Parte3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Ventana1 v1 = new Ventana1();
+        v1.setVisible(true);
+        
+        Ventana2 v2 = new Ventana2();
+        v2.setVisible(true);
+        
+        Ventana3 v3 =  new Ventana3();
+        v3.setVisible(true);
+        
+        Ventana4 v4 =  new Ventana4();
+        v4.setVisible(true);
+        
+        Ventana5 v5 =  new Ventana5();
+        v5.setVisible(true);
+        
+        
+        
+        
+    }
+    
+}
